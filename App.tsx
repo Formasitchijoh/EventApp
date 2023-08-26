54a9d5462b231f3a19a5423b6ea5f9b77bbc22e8
Formasitchijoh <formasitf@gmail.com>
2023-08-26 04:02:21 +0100

    created new branch

--- App.tsx
+++ App.tsx
@@ -7,32 +7,17 @@
 
 import React from 'react';
 import 'react-native-gesture-handler';
-import {Button, Text, View} from 'react-native';
 import {Login} from './src/Screens/Login/login';
 import Header from './src/component/Header/header';
-import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator} from '@react-navigation/native-stack';
-import { createStackNavigator } from '@react-navigation/stack';
-import Providers from './src/navigation';
-import CreateEvent from './src/component/createEvent';
-import HomeScreen from './src/Test/HomeScreen';
-import LoginScreen from './src/Test/LoginScreen';
-import { NavigationProp } from '@react-navigation/native';
-import gestureHandlerRootHOC, { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StackNavigationProp } from '@react-navigation/stack';
+
 import { SignUp } from './src/Screens/SignUp/SignUp';
 import { Home } from './src/Screens/Home/home';
 import AddEvent from './src/Screens/AddEvent/AddEvent';
 import { Participants } from './src/Screens/Participants/participants';
-import {PassingParametersToRoutes_HomeScreen,PassingParametersToRoutes_DetailScreen,PassingParametersToRoutes_SetParams} from './src/React_Navigation/PassingParametersToRoutes';
-import { Home_,Profile,Settings,Feed,Messages } from './src/React_Navigation/PassingParametersToRoutes';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { FormikComponent } from './src/component/Form_Input';
-import SignUpScreen from './src/Test/SignupScreen';
-import {Blog} from './src/component/BlogForm';
-import { Avatar } from './src/component/ImageGallery/Avatar';
+
 // import { ImageUpload } from './src/component/ImageUpload';
 const Tab = createBottomTabNavigator()
 const Stack = createNativeStackNavigator()
@@ -98,7 +83,7 @@ function App(): JSX.Element {
     
     }}
     initialRouteName="Login">
-     <Stack.Screen name='Login' options={{headerShown:false}} component={AddEvent}/>
+     <Stack.Screen name='Login' options={{headerShown:false}} component={Login}/>
         <Stack.Screen name='SignUp' options={{headerShown:false}} component={SignUp}/>
           <Stack.Screen name='TabScreens' options={{headerShown:false}} component={Tabs}/>
       </Stack.Navigator>
